d3695619759914e21985329fa34a82e2ee37a6bf
Umang <umangposhiya123@gmail.com>
2021-07-22 10:47:42 +0530

    trying database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const bodyparser = require("body-parser");
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://umangposhiya:Umang@1408@cluster0.97hfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://umangposhiya:Umang@1408@cluster0.97hfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true , useUnifiedTopology: true});
 
 
 const app = express();
