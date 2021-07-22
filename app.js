49b00245ea390d44dd81b1c7671460a53e106440
Umang <umangposhiya123@gmail.com>
2021-07-22 09:54:54 +0530

    adding database support

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const bodyparser = require("body-parser");
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/feedback', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://umangposhiya:Umang@1408@cluster0.97hfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true , useUnifiedTopology: true});
 
 
 const app = express();
